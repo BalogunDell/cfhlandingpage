34f55f3c306926ebdc2b6e87751bbdcb3b10b702
Andela Developer <delighteddell@gmail.com>
2017-11-02 17:50:27 +0100

    add modified files

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,9 +12,25 @@ $(document).ready(function(){
         var scrollAmount = $(window).scrollTop()
         if(scrollAmount > 50) {
             $('.navbar').addClass('navbar-scroll');
+            $('.navbar-brand-fixed').addClass('navbar-brand-animate');
+            $('.nav-link').addClass('nav-link-animate');
+            $('.nav').addClass('link-adjust');
+            $('#image').removeClass('initialLogo');
+            $('#image').addClass('finalLogo');
         } else {
             $('.navbar').removeClass('navbar-scroll');
+            $('.navbar-brand-fixed').removeClass('navbar-brand-animate');
+            $('.nav-link').removeClass('nav-link-animate');
+            $('.nav').removeClass('link-adjust');
+            $('#image').addClass('initialLogo');
+            $('#image').removeClass('finalLogo');
+
         }
     });
-    
+
+    // Hide nabvar on click
+
+    $('nav').find('li').bind('click' ,'a' ,function(){
+        $(this).collapse('hide');
+    });
 });
